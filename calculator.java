a84f377dadc3a49b77ea083a9bc8e4d781c340e2
deepajadhav1107 <deepajadhav2001@gmail.com>
2025-04-17 14:14:35 +0530

    save

--- calculator.java
+++ calculator.java
@@ -1,49 +1,45 @@
 import java.util.Scanner;
 
-public class calculator extends ExtendedArithmeticOperation{
+public class calculator extends ExtendedArithmeticOperation {
 
-    public static float addition(float op1,float op2)
-    {
+    public static float addition(float op1, float op2) {
         return op1 + op2;
     }
 
-    public static float substraction(float op1,float op2)
-    {
+    public static float subtraction(float op1, float op2) {
         return op1 - op2;
     }
-    public static float multiplication(float op1,float op2)
-    {
+
+    public static float multiplication(float op1, float op2) {
         return op1 * op2;
     }
-    public static float division(float op1,float op2)
-    {
+
+    public static float division(float op1, float op2) {
+        if (op2 == 0) {
+            System.out.println("Error: Division by zero.");
+            return 0;
+        }
         return op1 / op2;
     }
 
-    public static void main(String[] args) {
-        
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter two number\n");
-
-        // The code `float num1 = in.nextFloat();` and `float num2 = in.nextFloat();` is used to read
-        // two floating-point numbers from the user's input. The `in.nextFloat()` method reads the next
-        // float value entered by the user and assigns it to the variables `num1` and `num2`
-        // respectively.
-
-        // commenting user input to run jenkins job 
-        float num1 = 20;//in.nextFloat();
-        float num2 = 30;//in.nextFloat();
-
-        System.out.println("Addition of Two number:"+addition(num1, num2));
-        System.out.println(" Subtration of Two number:"+substraction(num1, num2));
-        System.out.println("Multiplication of Two number:"+multiplication(num1, num2));
-        System.out.println("Division of Two number:"+division(num1, num2));
-        System.out.println("Square Root of number:"+SquareRoot(num1));
-
-
-
-
+    public static double squareRoot(float num) {
+        return Math.sqrt(num);
     }
-    
-    
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter two numbers:\n");
+
+        float num1 = 20; // in.nextFloat();
+        float num2 = 30; // in.nextFloat();
+
+        System.out.println("Addition: " + addition(num1, num2));
+        System.out.println("Subtraction: " + subtraction(num1, num2));
+        System.out.println("Multiplication: " + multiplication(num1, num2));
+        System.out.println("Division: " + division(num1, num2));
+        System.out.println("Square Root of num1: " + squareRoot(num1));
+
+        in.close();
+    }
 }
